5997bb1bf1be451abb0564fbae88b5a91554b605
Lucas Alencar <alencar.lucas.a@gmail.com>
2021-06-19 16:02:16 -0300

    Perform npm install when there is no node_modules

--- sdk/webhooks/ActionsOnGoogleFulfillment/scripts/test.sh
+++ sdk/webhooks/ActionsOnGoogleFulfillment/scripts/test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.google-cloud-credentials/barista-3b2b6-5137c11445fb.json
+
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
+
 npm test
