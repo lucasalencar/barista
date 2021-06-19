77952b902059355eb322a315d7917f3dc3f73ece
Lucas Alencar <alencar.lucas.a@gmail.com>
2021-06-19 16:21:16 -0300

    Add reminder to test script

--- sdk/webhooks/ActionsOnGoogleFulfillment/scripts/test.sh
+++ sdk/webhooks/ActionsOnGoogleFulfillment/scripts/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -euo pipefail
 
-export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.google-cloud-credentials/barista-3b2b6-5137c11445fb.json
+echo 'REMINDER: Make sure you have deployed through Google Actions interface accessing Test page.'
 
 if [ ! -d "node_modules" ]; then
     npm install
 fi
 
+export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.google-cloud-credentials/barista-3b2b6-5137c11445fb.json
 npm test
