4a19ca1736bcc8ffe8481cda5eb7fb10b2240995
Lucas Alencar <alencar.lucas.a@gmail.com>
2021-08-15 15:40:59 -0300

    Add intro to afternoon coffee

--- sdk/webhooks/ActionsOnGoogleFulfillment/index.js
+++ sdk/webhooks/ActionsOnGoogleFulfillment/index.js
@@ -47,6 +47,8 @@ app.handle('v60_timer', (conv) => {
 });
 
 app.handle('v60_timer_afternoon', (conv) => {
+  conv.add('Receita para 20 gramas de café.');
+
   conv.add(helpers.v60_timer_recipe(
     absolute_timing_seconds,
     timing_discounts,
